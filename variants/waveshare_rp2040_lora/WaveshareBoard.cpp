e11e64c3b72fc5c3684c6f651fe080aa4b82b4d8
Malkavien <themalkavien@gmail.com>
2026-02-09 08:12:56 +0100

    Use Macros

--- variants/waveshare_rp2040_lora/WaveshareBoard.cpp
+++ variants/waveshare_rp2040_lora/WaveshareBoard.cpp
@@ -2,8 +2,6 @@
 
 #include <Arduino.h>
 #include <Wire.h>
-#include <hardware/vreg.h>
-#define CLOCK_MHZ 48
 
 void WaveshareBoard::begin() {
   // for future use, sub-classes SHOULD call this from their begin()
@@ -22,37 +20,29 @@ void WaveshareBoard::begin() {
   Wire.setSCL(PIN_BOARD_SCL);
 #endif
 
-  vreg_set_voltage(VREG_VOLTAGE_0_90);
-      /* Set the system frequency to 18 MHz. */
-  set_sys_clock_khz(CLOCK_MHZ * KHZ, false);
-  /* The previous line automatically detached clk_peri from clk_sys, and
-      attached it to pll_usb. We need to attach clk_peri back to system PLL to keep SPI
-      working at this low speed.
-      For details see https://github.com/jgromes/RadioLib/discussions/938
-  */
-  clock_configure(clk_peri,
-                  0,                                                // No glitchless mux
-                  CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLK_SYS, // System PLL on AUX mux
-                  CLOCK_MHZ * MHZ,                                         // Input frequency
-                  CLOCK_MHZ * MHZ                                          // Output (must be same as no divider)
-  );
-  /* Run also ADC on lower clk_sys. */
-  clock_configure(clk_adc, 0, CLOCKS_CLK_ADC_CTRL_AUXSRC_VALUE_CLKSRC_PLL_SYS, 48 * MHZ, 48 * MHZ);
-  /* Run RTC from XOSC since USB clock is off */
-  clock_configure(clk_rtc, 0, CLOCKS_CLK_RTC_CTRL_AUXSRC_VALUE_XOSC_CLKSRC, 12 * MHZ, 46875);
-
-
-digitalWrite(22, LOW); // ensure ESP32Flasher is not woken up by default
   Wire.begin();
 
   delay(10); // give sx1262 some time to power up
+
+  #ifdef MLK_RP2040_LOWPOWER
+    rp2040_lowpower();
+  #endif
+  #ifdef MLK_ESP32_FLASHER_PIN
+    pinMode(MLK_ESP32_FLASHER_PIN, OUTPUT);
+    digitalWrite(MLK_ESP32_FLASHER_PIN, LOW); // ensure ESP32Flasher is not woken up by default
+  #endif
 }
 
 bool WaveshareBoard::startOTAUpdate(const char *id, char reply[]) {
-    pinMode(22, OUTPUT);
-    digitalWrite(22, HIGH); // wake up esp32flasher
+  #ifdef MLK_ESP32_FLASHER_PIN
+    pinMode(MLK_ESP32_FLASHER_PIN, OUTPUT);
+    digitalWrite(MLK_ESP32_FLASHER_PIN, HIGH); // wake up esp32flasher
     delay(500);
-    digitalWrite(22, LOW); // stay low to avoid another wake up
+    digitalWrite(MLK_ESP32_FLASHER_PIN, LOW); // stay low to avoid another wake up
     sprintf(reply, "Waking UP ESP32Flasher to flash the firmware.");
   return true;
+  #else
+    sprintf(reply, "OTA update not supported on this board");
+    return false;
+  #endif
 }
--- variants/waveshare_rp2040_lora/WaveshareBoard.h
+++ variants/waveshare_rp2040_lora/WaveshareBoard.h
@@ -59,3 +59,29 @@ public:
 
   bool startOTAUpdate(const char *id, char reply[]) override;
 };
+
+#ifdef MLK_RP2040_LOWPOWER
+#include <hardware/vreg.h>
+#define CLOCK_MHZ 48
+
+inline void rp2040_lowpower() {
+  vreg_set_voltage(VREG_VOLTAGE_0_90);
+      /* Set the system frequency to 18 MHz. */
+  set_sys_clock_khz(CLOCK_MHZ * KHZ, false);
+  /* The previous line automatically detached clk_peri from clk_sys, and
+      attached it to pll_usb. We need to attach clk_peri back to system PLL to keep SPI
+      working at this low speed.
+      For details see https://github.com/jgromes/RadioLib/discussions/938
+  */
+  clock_configure(clk_peri,
+                  0,                                                // No glitchless mux
+                  CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLK_SYS, // System PLL on AUX mux
+                  CLOCK_MHZ * MHZ,                                         // Input frequency
+                  CLOCK_MHZ * MHZ                                          // Output (must be same as no divider)
+  );
+  /* Run also ADC on lower clk_sys. */
+  clock_configure(clk_adc, 0, CLOCKS_CLK_ADC_CTRL_AUXSRC_VALUE_CLKSRC_PLL_SYS, 48 * MHZ, 48 * MHZ);
+  /* Run RTC from XOSC since USB clock is off */
+  clock_configure(clk_rtc, 0, CLOCKS_CLK_RTC_CTRL_AUXSRC_VALUE_XOSC_CLKSRC, 12 * MHZ, 46875);
+}
+#endif
