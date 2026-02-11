160229bc6660c6c29a50aad06b0164c30d696b1d
Malkavien <themalkavien@gmail.com>
2026-02-11 21:44:05 +0100

    Initial commit.

--- examples/companion_radio/MyMesh.h
+++ examples/companion_radio/MyMesh.h
@@ -166,6 +166,7 @@ protected:
 
 public:
   void savePrefs() { _store->savePrefs(_prefs, sensors.node_lat, sensors.node_lon); }
+  void persistChannels() { _store->saveChannels(this); }
 
 #if ENV_INCLUDE_GPS == 1
   void applyGpsPrefs() {
