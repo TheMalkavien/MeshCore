c00644e446b8e6d4233324740172cc0b88cf23e8
Malkavien <themalkavien@gmail.com>
2026-02-15 17:18:50 +0100

    fixes multiple builds, sensors, etc

--- variants/waveshare_rp2040_lora/target.h
+++ variants/waveshare_rp2040_lora/target.h
@@ -13,7 +13,11 @@
 extern WaveshareBoard board;
 extern WRAPPER_CLASS radio_driver;
 extern AutoDiscoverRTCClock rtc_clock;
+#ifdef MLK_SENSORS
 extern EnvironmentSensorManager sensors;
+#else
+extern SensorManager sensors;
+#endif
 
 bool radio_init();
 mesh::LocalIdentity radio_new_identity();
