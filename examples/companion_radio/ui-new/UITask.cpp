3b6e5d9292001b8a7b6415bb74fd1f25be6482a6
Malkavien <themalkavien@gmail.com>
2026-02-23 19:55:29 +0100

    more more power

--- examples/companion_radio/ui-new/UITask.cpp
+++ examples/companion_radio/ui-new/UITask.cpp
@@ -555,7 +555,7 @@ public:
 #if AUTO_OFF_MILLIS==0 // probably e-ink
     return 10000; // 10 s
 #else
-    return 1000;  // next render after 1000 ms
+    return 3000;  // next render after 3000 ms
 #endif
   }
 
