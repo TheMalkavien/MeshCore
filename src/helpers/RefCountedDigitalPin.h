ad1c4822fa853a44bef135f3cbb86cac4b401eb6
Malkavien <themalkavien@gmail.com>
2026-02-23 12:47:17 +0100

    More power

--- src/helpers/RefCountedDigitalPin.h
+++ src/helpers/RefCountedDigitalPin.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <Arduino.h>
+#if defined(ESP_PLATFORM) && defined(CONFIG_PM_SLP_DISABLE_GPIO)
+  #include <driver/gpio.h>
+#endif
 
 class RefCountedDigitalPin {
   uint8_t _pin;
@@ -12,6 +15,11 @@ public:
   void begin() {
     pinMode(_pin, OUTPUT);
     digitalWrite(_pin, !_active);  // initial state
+#if defined(ESP_PLATFORM) && defined(CONFIG_PM_SLP_DISABLE_GPIO)
+    // Keep this GPIO under normal control in auto light sleep.
+    // Required for power-gate pins (e.g. VEXT for OLED/GPS rails).
+    gpio_sleep_sel_dis((gpio_num_t)_pin);
+#endif
   }
 
   void claim() {
