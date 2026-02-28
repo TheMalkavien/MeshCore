99827765e7a7cd90f6664e09f5ba642bcf8819f6
Malkavien <themalkavien@gmail.com>
2026-02-28 17:50:53 +0100

    more optim

--- examples/companion_radio/MyMesh.cpp
+++ examples/companion_radio/MyMesh.cpp
@@ -481,6 +481,7 @@ bool MyMesh::filterRecvFloodPacket(mesh::Packet* packet) {
 }
 
 bool MyMesh::allowPacketForward(const mesh::Packet* packet) {
+  (void)packet;
   return _prefs.client_repeat != 0;
 }
 
